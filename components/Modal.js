c96446e08668b87cdccb297233046c6c4db46ec0
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-10-31 05:31:32 +0100

    form

--- components/Modal.js
+++ components/Modal.js
@@ -3,17 +3,29 @@ import "./modal.css"
 
 const Modal = ({closeModal}) => {
   return (
-    <div className='modalBackground'>
+    <div className='modalBackground font-raleway'>
         <div className='modalContainer'>
-            <button onClick={() => closeModal(false)}>X</button>
-            <div className='title'>
-                <h1>Are you sure you want to continue?</h1>
-            </div>
-            <div className='body'>
-                <p></p>
-            </div>
-            <div className='footer'>
-                <button>cancel</button>
+           
+            <div className='flex flex-row items-start justify-between'>
+                <div>
+                    <div className='flex flex-col gap-1'>
+                    <h1 className='uppercase text-xl font-bold tracking-wider'> Schedule a call with us</h1>
+                    <p>*All fields are required.</p>
+                    </div>
+
+                    <div>
+                        <form className='mb-0 space-y-6' action='#' method='POST'>
+                            <div>
+                                
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div>
+                     <button className='py-1 px-3 font-bold border-2 border-gray-500'  onClick={() => closeModal(false)}>
+                     <i class="ri-close-fill"></i>
+                     </button>
+                </div>
             </div>
         </div>
     </div>
